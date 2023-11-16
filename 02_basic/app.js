2a781f92efd5517a56a859e682fc787c3f7f9095
Imshyeon <kzoen0040@naver.com>
2023-11-16 16:51:20 +0900

    렌더링 방법 2

--- 02_basic/app.js
+++ 02_basic/app.js
@@ -7,11 +7,28 @@ scene.background = new THREE.Color(0x004fff); // 컬러 바꾸기
 // 카메라 (화각, 종횡비, 가까운 정도)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+// 캔버스
+const canvas = document.querySelector('#app');
+const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+
 // 렌더러
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
 
 // 방법 1
-document.body.appendChild(renderer.domElement);
+// document.body.appendChild(renderer.domElement);
+
+function render(time) {
+    time *= 0.001;  // convert time to seconds
+   
+    // cube.rotation.x = time;
+    // cube.rotation.y = time;
+   
+    renderer.render(scene, camera);
+   
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+
 renderer.render(scene, camera);
 
